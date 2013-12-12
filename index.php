814fe376c669175c6d2235ee51cf66c9e0f0249d
Phil 'The Guru' Wright- Christie <philwc@gmail.com>
2013-12-12 14:30:07 +0000

    Update example

--- index.php
+++ index.php
@@ -2,19 +2,25 @@
 $loader = require __DIR__ . '/vendor/autoload.php';
 $loader->add('philwc', __DIR__ . '/src');
 $change = new \philwc\Web\AddChange();
-$fields = $change->getFields();
 
-if(isset($_POST['submit'])){
-    foreach($fields as $field){
-        if(isset($_POST[$field])){
-            $methodName = 'set'.ucwords($field);
+if (isset($_POST['submit'])) {
+    $commitOk = true;
+    foreach ($change->getFields() as $field) {
+        if (isset($_POST[$field]) && $_POST[$field] !== '') {
+            $methodName = 'set' . ucwords($field);
             $change->$methodName($_POST[$field]);
+        } else {
+            $commitOk = false;
         }
     }
-    $change->commit();
-    echo 'Change Committed';
-    echo '<a href="'.$_SERVER['PHP_SELF'].'">Back</a>';
-}else{
+    if ($commitOk) {
+        $change->commit();
+        echo 'Change Committed';
+        echo '<a href="' . $_SERVER['PHP_SELF'] . '">Back</a>';
+    } else {
+        echo 'Required Fields Not Completed';
+    }
+} else {
     echo $change->getHtml($_SERVER['PHP_SELF']);
 }
 
